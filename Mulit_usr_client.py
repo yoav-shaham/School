af6e483954c65ea769f13a8af14485b64355d293
yoav-shaham <shaham.yoav2000@gmail.com>
2017-09-02 14:39:13 +0300

    fixing and finished up all of it

--- Mulit_usr_client.py
+++ Mulit_usr_client.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 
 name=raw_input("Please insert your name: ")#getting client name
+while len(name.split(" "))>=2:
+    name = raw_input("Please insert your name in a single word: ")  # getting client name
 ip = raw_input("please insert the ip of the desired server: ")#getting sever ip adress
 port = raw_input("please insert the port of the desired server: ")#getting server port
 the_socket = socket.socket()
@@ -14,7 +16,7 @@ try:
 
 except:
     exit("Something went wrong exiting")#failed to connect
-print "Please insert the desired message or press on enter to close the connection: "#
+print "Please insert the desired message or press send >>exit to close the connection: "#
 
 def message_sender(message):
     if message == ">>exit":
@@ -27,6 +29,8 @@ def message_sender(message):
 
 def recive_data():
     recived_data = the_socket.recv(1024)
+    if len(recived_data)==0 or recived_data=="":
+        exit("you were disconnected from the server")
     recived_data= recived_data.split("\r\n")#reciving the information and spliting it
     return recived_data
 
@@ -42,10 +46,13 @@ def main():
     while True:
         rlist, wlist, xlist = select.select([the_socket],[the_socket],[])
         if the_socket in rlist:
-            data = recive_data()
+            try:
+                data = recive_data()
+            except:
+                exit(1)
             if data[0]==None:
                 pass
-            elif data[1] == "":#checking if you were kicked off the server
+            elif len(data)==0:#checking if you were kicked off the server
                 exit(data[0]+" closed the connection or kicked you off")
             else:
                 print "\r\n"+data[0]+": "+data[1]#printing the sent messge
--- Multi_Usr_Sever.py
+++ Multi_Usr_Sever.py
@@ -15,11 +15,15 @@ socket_dic={}
 muted_soc=[]
 def remove_user(command):
     name = command[1]
+    print name
     kicking_socket=list(socket_dic.keys())[list(socket_dic.values()).index(name)]
     if len(command)>2:
         reason=command[2::]
-        reason.join(" ")
-        time_now_kicking = str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
+        reason=" ".join(reason)
+        try:
+            time_now_kicking = str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
+        except:
+            return
         kicking_socket.send(time_now_kicking+"server\r\nYou have been kicked by an admin he gave this reason: "+reason)
     else:
         time_now_kicking = str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
@@ -32,15 +36,18 @@ def remove_user(command):
         messages_to_send.append([client_socket, new_message])
     temp_client_sockets = []
     send_waiting_messages()
-    kicking_socket.close
+    kicking_socket.close()
+    open_client_sockets.remove(kicking_socket)
 
 
 
 
 def new_manager(command):
+    global admins
     name=command[1]
     if name not in admins:
         admins.append(name)
+    print admins
 
 def mute(command):
     #length is in min
@@ -84,8 +91,25 @@ def check_list(target_socket):
     return None
 
 
+def private_message(senders_name,new_message):
+    message=" ".join(new_message[2::])
+    name=new_message[1]
+    send_to_socket = list(socket_dic.keys())[list(socket_dic.values()).index(name)]
+    time_now = str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
+    send_to_socket.send(time_now+senders_name+"\r\nthis is a private message"+message)
+
 def data_reader(current_socket):
-    data = current_socket.recv(1024)
+    try:
+        data = current_socket.recv(1024)
+    except:
+        open_client_sockets.remove(current_socket)
+        name=socket_dic[current_socket]
+        curent_time= str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
+        temp_client_sockets = open_client_sockets[::]
+        for client_socket in temp_client_sockets:
+            messages_to_send.append([client_socket,curent_time+name+"\r\n"+ "has left the server" ])
+
+
     data2=data[::]
     time_sent,name,message = data.split("\r\n")#readiung the messge from the socket
     if name in admins:
@@ -103,6 +127,12 @@ def data_reader(current_socket):
         for client_socket in temp_client_sockets:
             messages_to_send.append([client_socket, data])
         temp_client_sockets = []
+        return
+    elif ">>private" in message and len(message.split(" "))>=3:
+        new_message=message.split(" ")
+        if new_message[1] in socket_dic.values():
+            private_message(name,new_message)
+            return
     if "@" in name or name in admins:
         print "entered admin commands section"
         print message
@@ -122,17 +152,22 @@ def data_reader(current_socket):
                 mute(new_message)
                 print "something went right"
                 return
-        if ">>new_manager" in message and len(message.split(" "))>1:
+        if ">>new_manager" in message and len(message.split(" "))>=2:
+            print "inside the manager"
             new_message = message.split(" ")
             if new_message[0]==">>new_manager":
-                new_manager(message)
+                print "creating a new manager"
+                new_manager(new_message)
+                return
         if ">>remove_user" in message and len(message.split(" "))>1:
             new_message = message.split(" ")
             if new_message[0]==">>remove_user" and new_message[1] not in admins:
-                remove_user(message)
+                remove_user(new_message)
+                return
             if new_message[1] in admins:
                 time_now_kicking = str(datetime.now().time().hour) + ":" + str(datetime.now().time().minute) + " "
                 messages_to_send.append([current_socket,time_now_kicking + "server\r\nyou cannot kick an admin"])
+                return
 
     if check_list(current_socket)==None:
         print "got out of admin command center1"
