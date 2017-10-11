6dd4f3d637b6d2966f5f6340353845f1b8911687
yoav-shaham <shaham.yoav2000@gmail.com>
2017-10-11 14:23:37 +0300

    Finished HexDump command starting with Grep command now

--- Shell/Shell.py
+++ Shell/Shell.py
@@ -1,34 +1,40 @@
-__author__="Yoav Shaham"
+__author__ = "Yoav Shaham"
 import getpass
 import os
+import binascii
+
+def read_binary(path):
+    file=open(path,"rb").read()
+    file=binascii.hexlify(file)
+    return file
+
+
 def get_command(input):
-    input =input.split(" ")
-    command=input[0]
-    if command=="hello":
+    input = input.split(" ")
+    command = input[0]
+    if command == "hello":
         return getpass.getuser()
-    elif command== "HexDump":
-        if len(input)<=1:
+    elif command == "HexDump":
+        if len(input) <= 1:
             return "To Use HexDump You Need To Give A Path"
         else:
-            path=input[1]
+            path = input[1]
             if not os.path.isfile(path) or not path.endswith(".bin"):
                 return "To Use HexDump You Need To Give A Real Path to a Binary File"
             else:
-                pass
-    elif command =="grep":
-        pass
+                return read_binary(path)
+    elif command == "grep":
+
     else:
         return "Not viable command"
 
 
 def main():
-    input= raw_input()
-    while input!="exit":
+    input = raw_input()
+    while input != "exit":
         print get_command(input)
-        input=raw_input()
+        input = raw_input()
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
