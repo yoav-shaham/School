9df539ac2344e0a764a1ad2517c029eb3ba6797d
yoav-shaham <shaham.yoav2000@gmail.com>
2017-10-11 13:44:17 +0300

    creating shell.py file

--- decryption/decryption.py
+++ decryption/decryption.py
@@ -1,19 +1,25 @@
 __author__ = 'Cyber-01'
 import md5
 import multiprocessing
-
+import numba
 encrypted="EC9C0F7EDCC18A98B1F31853B1813301".lower()
 flag=0
-def decryptor(start, end):
+
+def decryptor(info):
+    start=info[0]
+    end=info[1]
     global flag
     percentage=0
     jumps=(end-start)/100
     recorder=jumps
     while start < end and flag == 0:
+
         md5format = md5.md5(str(start)).hexdigest()
+
         if md5format == encrypted:
             print str(start)+"hello"
             flag=1
+            return str(start)
             exit()
         start += 1
         if recorder<=0 :
@@ -29,20 +35,27 @@ def divider(parts, start , end):
     for times in range(parts):
         sections.append([start,start+jump])
         start=start+jump
-    if start != end:
-        sections.append([start,end])
+    #if start != end:
+        #sections.append([start,end])
+    #print sections
     return sections
 
+@numba.jit
 def main():
+    print encrypted
     procecess=[]
     start = 10000000000
     end = 99999999999
-    devision=divider(10,start,end)
-    for mission in devision:
-        procecess.append(multiprocessing.Process(target=decryptor,args=(mission[0],mission[1])))
+    devision=divider(8,start,end)
+    pool= multiprocessing.Pool()
 
-    for proce in procecess:
-        proce.start()
+    x=[pool.map_async(decryptor,devision)]
+    print x[0].get()
+    #for mission in devision:
+      #  procecess.append(multiprocessing.Process(target=decryptor,args=(mission[0],mission[1])))
+
+    #for proce in procecess:
+     #   proce.start()
 
 
 if __name__=="__main__":
