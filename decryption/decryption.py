855fd1257005942261b28abe3afe6d541625e8f1
yoav-shaham <shaham.yoav2000@gmail.com>
2017-10-19 09:57:04 +0300

    Update decryption.py
    FINISHED DECRYTPTION JUST NEED TO RUN

--- decryption/decryption.py
+++ decryption/decryption.py
@@ -1,33 +1,33 @@
 __author__ = 'Cyber-01'
-import md5
+import hashlib
 import multiprocessing
-import numba
+import myglobals
 encrypted="EC9C0F7EDCC18A98B1F31853B1813301".lower()
-flag=0
+flag=None
 
-def decryptor(info):
-    start=info[0]
-    end=info[1]
+def init(args):
     global flag
+    flag=args
+def decryptor(info):
+    print(encrypted)
+    start=long(info[0])
+    end=long(info[1])
     percentage=0
     jumps=(end-start)/100
     recorder=jumps
-    while start < end and flag == 0:
-
-        md5format = md5.md5(str(start)).hexdigest()
+    global flag
+    while start < end and flag.value == 0:
+        md5format = hashlib.md5(str(start)).hexdigest()
 
         if md5format == encrypted:
-            print str(start)+"hello"
-            flag=1
+            flag.value=1
             return str(start)
-            exit()
         start += 1
         if recorder<=0 :
             percentage+=1
             print percentage
             recorder=jumps
         recorder-=1
-        start += 1
 def divider(parts, start , end):
     jump= end- start
     jump=jump/parts
@@ -35,28 +35,21 @@ def divider(parts, start , end):
     for times in range(parts):
         sections.append([start,start+jump])
         start=start+jump
-    #if start != end:
-        #sections.append([start,end])
-    #print sections
     return sections
 
-@numba.jit
 def main():
+    myglobals.data=0
     print encrypted
+
     procecess=[]
-    start = 10000000000
-    end = 99999999999
-    devision=divider(8,start,end)
-    pool= multiprocessing.Pool()
-
-    x=[pool.map_async(decryptor,devision)]
-    print x[0].get()
-    #for mission in devision:
-      #  procecess.append(multiprocessing.Process(target=decryptor,args=(mission[0],mission[1])))
-
-    #for proce in procecess:
-     #   proce.start()
-
+    start = 1000000000
+    end = 9999999999
+    devision=divider(8,start,end,)
+    pool= multiprocessing.Pool(initializer = init, initargs = (flag, ))
+    x=pool.map_async(decryptor,devision,chunksize = 1)
+    print x.wait()
+    print x.get()
 
 if __name__=="__main__":
+    flag = multiprocessing.Value('i', 0)
     main()
