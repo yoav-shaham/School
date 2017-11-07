8192203756360bdb3f0cccc78a8a80c9b5776c12
tomer <shaham.yoav2000@gmail.com>
2017-11-07 09:46:27 +0200

    project test

--- Final Project/Trial.py
+++ Final Project/Trial.py
@@ -1,33 +1,32 @@
-import requests
-import json
+import io
+import os
 
-url = "https://speech.googleapis.com/v1/speech:longrunningrecognize?"
-key=r"AIzaSyDtKtpqWrJoCGShlr_fs-Bu5AVFdsZ9AL0"
-payload = {
-    "config": {
-      "encoding": "LINEAR16",
-      "sample_rate_hertz": 48000,
-      "language_code": "en-US"
-    },
-    "audio": {
-        "uri": "gs://bucket45858/sound.mp3"
-    }
-}
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
+# Instantiates a client
+client = speech.SpeechClient()
 
-def main():
-    global url
-    r = requests.post(url, data=json.dumps(payload))
-    print r.json()
-    # json_resp = r.json()
-    # token_resp = json_resp['']
-    # url = "https://speech.googleapis.com/v1/operations/" + str(token_resp) + key
-    # content_response = requests.get(url)
-    # content_json = content_response.json()
-    # print content_json
-    # print content_response
-    # print token_resp
-    # print r
+# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.raw')
 
-if __name__=="__main__":
-    main()
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
